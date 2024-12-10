6bc8b59a8c59c28161c6910e82bfa25300ce0178
Chaitanya Katare <chaitanyask103@gmail.com>
2024-12-10 19:15:56 +0530

    commit

---
+++ app/src/main/java/com/androidkotlin/recipeapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.androidkotlin.recipeapp
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.androidkotlin.recipeapp.ui.theme.RecipeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RecipeAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RecipeScreen()
+                }
+            }
+        }
+    }
+}
